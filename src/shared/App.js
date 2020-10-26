f5cfedfe1c59d58d81f9d590d0978827f0a6af37
SungJin <esung246@gmail.com>
2020-10-26 16:00:53 +0900

    [Edit] app
    Header page in app.js

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import Header from './../components/Header/Header';
 
 function App() {
   return (
     <div>
-      hello
+      <Header></Header>
     </div>
   );
 }
