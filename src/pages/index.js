8f8f19d0f73f2e1e91e97678187e6700a45411b6
SungJin <esung246@gmail.com>
2020-11-16 23:52:39 +0900

    [Edit] develope merge Edit #1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
+export {default as Main} from './Main';
 export {default as signUp} from './signUp';
-export {default as Forgot} from './Forgot';
+export { default as Forgot } from './Forgot';
 export { default as Schedule } from './Schedule';
 export { default as Kanban } from './Kanban';
 export { default as Roadmap } from './Roadmap';
 export { default as Setting } from './Setting';
 export { default as Profile } from './Profile';
-
-export {default as Profile} from './Profile';
-export {default as setProject} from './setProject';
+export { default as Profile } from './Profile';
+export { default as setProject } from './setProject';
--- src/shared/App.js
+++ src/shared/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
-import { Schedule, Kanban, Roadmap, Setting, Profile } from './../pages';
-import {Main, signUp, Forgot, Profile, setProject} from './../pages/index'
+import { Route } from 'react-router-dom';
+import { Schedule, Kanban, Roadmap, Setting, Profile, Main, signUp, Forgot, Profile, setProject } from './../pages';
 
 const App = () => {
     return(
@@ -9,11 +8,11 @@ const App = () => {
             <Route exact path="/" component={Main} />
             <Route path="/Join" component={signUp} />
             <Route path="/Forgot" component={Forgot} />
-      <Route path="/schedule" component={Schedule}/>
-      <Route path="/kanban" component={Kanban}/>
-      <Route path="/roadmap" component={Roadmap}/>
-      <Route path="/setting" component={Setting}/>
-      <Route path="/profile" component={Profile}/>
+            <Route path="/schedule" component={Schedule}/>
+            <Route path="/kanban" component={Kanban}/>
+            <Route path="/roadmap" component={Roadmap}/>
+            <Route path="/setting" component={Setting}/>
+            <Route path="/profile" component={Profile}/>
             <Route path = "/setProject" component = {setProject}/>
         </div>
     )
